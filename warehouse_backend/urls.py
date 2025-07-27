9391ced808438dbf7f44a4a9eebe6147001da6c9
laxmigupta31111 <laxmi07114@gmail.com>
2025-07-27 20:52:07 +0530

    Add root view to Django for Render deployment welcome page

--- warehouse_backend/urls.py
+++ warehouse_backend/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def root_view(request):
+    return HttpResponse("<h2>Welcome to the Warehouse Management System API.<br>Visit /api/ for API endpoints.</h2>")
 
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
 ]
